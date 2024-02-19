c7f3541281a477b31dba7d1d4b21f01b71b3d730
spiritevolution <158512066+spiritevolution@users.noreply.github.com>
2024-02-19 21:07:16 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+    }
+    return data;
+}
+function generateRandomData() {
+main();
+
+
+function main() {
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+    });
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+}
+    const data = [];
+    data.forEach(item => {
